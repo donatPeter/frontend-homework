f037dbe396b7575cdd8bf254ce11fba906e91f10
donatPeter <peterdonat.bme@gmail.com>
2021-07-04 18:20:30 +0200

    implement different states for company selector

--- src/components/CompanyLink.tsx
+++ src/components/CompanyLink.tsx
@@ -1,14 +1,26 @@
 import { Company, ReduxState } from "../types/types";
 import styled from "styled-components";
-import { connect, useSelector } from "react-redux";
-import { getSelectedCompanyId, isCompanySelected } from "../store/selectors";
+import { connect } from "react-redux";
+import { getSelectedCompanyId } from "../store/selectors";
 import { createStructuredSelector } from "reselect";
+import { setSelectedCompanyId } from "../store/actions";
 
 const StyledCompanyLinkName = styled.div<{ $isSelected: boolean }>`
   padding: 12px 17px;
   font-weight: bold;
   background: ${({ $isSelected }) =>
     $isSelected ? "rgba(55, 164, 71, 0.1)" : "unset"};
+  &:hover {
+    background: ${({ $isSelected }) =>
+      $isSelected ? "rgba(55, 164, 71, 0.2)" : "#f7f7f9"};
+  }
+  &:focus {
+    background: ${({ $isSelected }) =>
+      $isSelected ? "rgba(55, 164, 71, 0.2)" : "#f7f7f9"};
+  }
+  &:active {
+    background: #eeeef2;
+  }
 `;
 
 interface Props {
@@ -19,12 +31,22 @@ type ReduxProps = {
   selectedCompanyId: number | null;
 };
 
+type DispatchProps = {
+  setSelectedCompanyId: (id: number) => void;
+};
+
 const CompanyLink = ({
   company: { name, id },
   selectedCompanyId,
-}: Props & ReduxProps) => {
+  setSelectedCompanyId,
+}: Props & ReduxProps & DispatchProps) => {
+  const handleCompanySelect = () => setSelectedCompanyId(id);
+
   return (
-    <StyledCompanyLinkName $isSelected={selectedCompanyId === id}>
+    <StyledCompanyLinkName
+      $isSelected={selectedCompanyId === id}
+      onClick={handleCompanySelect}
+    >
       {name}
       <i />
     </StyledCompanyLinkName>
@@ -34,7 +56,6 @@ const CompanyLink = ({
 export default connect(
   createStructuredSelector<ReduxState, ReduxProps>({
     selectedCompanyId: getSelectedCompanyId,
-  })
+  }),
+  { setSelectedCompanyId }
 )(CompanyLink);
-
-// export default CompanyLink;
