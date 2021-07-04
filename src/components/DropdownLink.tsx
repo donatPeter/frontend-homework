5af87dc5c6f8f168dbe40ae36c0290f16678db06
donatPeter <peterdonat.bme@gmail.com>
2021-07-04 18:45:34 +0200

    show selected company name under username

--- src/components/DropdownLink.tsx
+++ src/components/DropdownLink.tsx
@@ -1,14 +1,20 @@
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
-import { ReduxState } from "../types/types";
-import { getIsDropdownMenuVisible } from "../store/selectors";
+import { Company, ReduxState } from "../types/types";
+import {
+  getCompanies,
+  getIsDropdownMenuVisible,
+  getSelectedCompanyId,
+} from "../store/selectors";
 import { toggleDropdownMenuVisibility } from "../store/actions";
 
 import DropdownMenu from "./DropdownMenu";
 
 type ReduxProps = {
   isDropdownMenuVisible: boolean;
+  companies: Array<Company>;
+  selectedCompanyId: number | null;
 };
 
 type DispatchProps = {
@@ -18,29 +24,37 @@ type DispatchProps = {
 export const DropdownLink = ({
   isDropdownMenuVisible,
   toggleDropdownMenuVisibility,
-}: ReduxProps & DispatchProps) => (
-  <>
-    <div
-      className="nav__link"
-      onClick={toggleDropdownMenuVisibility}
-      data-test-nav-link
-    >
-      <div className="nav__link-text-wrapper">
-        <div className="nav__link-text">Elon Musk</div>
+  companies,
+  selectedCompanyId,
+}: ReduxProps & DispatchProps) => {
+  const selectedCompanyName =
+    companies && companies.find((i) => i.id === selectedCompanyId)?.name;
+  return (
+    <>
+      <div
+        className="nav__link"
+        onClick={toggleDropdownMenuVisibility}
+        data-test-nav-link
+      >
+        <div className="nav__link-text-wrapper">
+          <div className="nav__link-text">Elon Musk</div>
 
-        <div className="nav__link-subtext">Viljatootja AS</div>
+          <div className="nav__link-subtext">{selectedCompanyName}</div>
+        </div>
+
+        <i className="material-icons-outlined nav__link-icon">settings</i>
       </div>
 
-      <i className="material-icons-outlined nav__link-icon">settings</i>
-    </div>
-
-    {isDropdownMenuVisible && <DropdownMenu />}
-  </>
-);
+      {isDropdownMenuVisible && <DropdownMenu />}
+    </>
+  );
+};
 
 export default connect(
   createStructuredSelector<ReduxState, ReduxProps>({
     isDropdownMenuVisible: getIsDropdownMenuVisible,
+    companies: getCompanies,
+    selectedCompanyId: getSelectedCompanyId,
   }),
   { toggleDropdownMenuVisibility }
 )(DropdownLink);
