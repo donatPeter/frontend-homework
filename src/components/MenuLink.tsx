db81822f20c3986d1689a63f2e049d7782a7aee4
donatPeter <peterdonat.bme@gmail.com>
2021-07-05 17:13:13 +0200

    use react-router-dom for routing

--- src/components/MenuLink.tsx
+++ src/components/MenuLink.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { Icon } from "./Icon";
+import { Link } from "react-router-dom";
 
 type Props = {
   text: string;
@@ -9,10 +10,10 @@ type Props = {
   color?: string;
 };
 
-export const StyledLink = styled.a<{ $color: string }>`
+export const StyledLink = styled(Link)`
   display: flex;
   text-decoration: none;
-  color: ${({ $color }) => $color};
+  color: ${(props) => props.color};
   align-items: center;
   padding: 6px 16px;
 `;
@@ -22,7 +23,7 @@ export const MenuText = styled.span`
 `;
 
 const MenuLink = ({ icon, text, href, color }: Props) => (
-  <StyledLink href={href} $color={color || "inherit"}>
+  <StyledLink to={href} color={color || "inherit"}>
     <Icon icon={icon} size="20px" />
     <MenuText>{text}</MenuText>
   </StyledLink>
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
 
 import App from "./App";
 import store from "./store/store";
+const history = createBrowserHistory();
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router history={history}>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById("root")
 );
